6aad0f7e6c979f30a1dff9ecc24724cae17f209e
Ramanpreet Singh <ramanpreet.singh@teambest.com>
2023-04-21 10:11:45 -0500

    added export functionality

--- gui/table/table.gd
+++ gui/table/table.gd
@@ -1,11 +1,14 @@
 extends VBoxContainer
 
+@onready var main : Node3D = get_node("/root/Node3D")
 @onready var tree : Tree = $Tree
 @onready var addButton : Button = $Buttons/Add
 @onready var editButton : Button = $Buttons/Edit
 @onready var saveButton : Button = $Buttons/Save
 @onready var deleteButton : Button = $Buttons/Delete
 @onready var clearButton: Button = $Clear
+@onready var importButton : Button = $FileOperations/Import
+@onready var exportButton : Button = $FileOperations/Export
 @onready var xslider : HSlider = get_node("/root/Node3D/GUI/Setter/X/slider")
 @onready var yslider : HSlider = get_node("/root/Node3D/GUI/Setter/Y/slider")
 @onready var zslider : HSlider = get_node("/root/Node3D/GUI/Setter/Z/slider")
@@ -18,9 +21,12 @@ extends VBoxContainer
 @onready var xdec : SpinBox = get_node("/root/Node3D/GUI/Setter/XOthers/dec")
 @onready var ydec : SpinBox = get_node("/root/Node3D/GUI/Setter/YOthers/dec")
 @onready var zdec : SpinBox = get_node("/root/Node3D/GUI/Setter/ZOthers/dec")
+@onready var duration : SpinBox = get_node("/root/Node3D/GUI/Setter/Common/duration")
 
 var selectedItem : TreeItem
 var root : TreeItem
+var importFile : String
+var exportFile : String
 
 func _ready():
 	# set column widths
@@ -44,6 +50,8 @@ func _ready():
 	saveButton.pressed.connect(_save_button_pressed)
 	deleteButton.pressed.connect(_delete_button_pressed)
 	clearButton.pressed.connect(_clear_button_pressed)
+	importButton.pressed.connect(_import_button_pressed)
+	exportButton.pressed.connect(_export_button_pressed)
 	# clear selection on ready
 	_clear_button_pressed()
 	
@@ -80,12 +88,19 @@ func _add_button_pressed():
 	subchild3.set_text(2, str(xdec.value))
 	subchild3.set_text(3, str(ydec.value))
 	subchild3.set_text(4, str(zdec.value))
+	# duration values
+	var subchild4 = child.create_child()
+	subchild4.set_text(1, "Dur:")
+	subchild4.set_text(2, str(duration.value))
+	subchild4.set_text(3, str(duration.value))
+	subchild4.set_text(4, str(duration.value))
 	# center align
 	for i in tree.columns:
 		child.set_text_alignment(i, HORIZONTAL_ALIGNMENT_CENTER)
 		subchild1.set_text_alignment(i, HORIZONTAL_ALIGNMENT_CENTER)
 		subchild2.set_text_alignment(i, HORIZONTAL_ALIGNMENT_CENTER)
 		subchild3.set_text_alignment(i, HORIZONTAL_ALIGNMENT_CENTER)
+		subchild4.set_text_alignment(i, HORIZONTAL_ALIGNMENT_CENTER)
 
 func _edit_button_pressed():
 	# get the selection
@@ -130,6 +145,10 @@ func _save_button_pressed():
 	selectedItem.get_child(2).set_text(2, str(xdec.value))
 	selectedItem.get_child(2).set_text(3, str(ydec.value))
 	selectedItem.get_child(2).set_text(4, str(zdec.value))
+	# dur values
+	selectedItem.get_child(3).set_text(2, str(duration.value))
+	selectedItem.get_child(3).set_text(3, str(duration.value))
+	selectedItem.get_child(3).set_text(4, str(duration.value))
 	_clear_button_pressed()
 
 func _delete_button_pressed():
@@ -152,3 +171,17 @@ func _clear_button_pressed():
 	editButton.disabled = true
 	deleteButton.disabled = true
 	saveButton.disabled = true
+	
+func _import_button_pressed():
+	pass
+
+func _export_button_pressed():
+	var exportDialog : FileDialog = get_node("/root/Node3D/GUI/FileDialog/Export")
+	exportDialog.show()
+	exportDialog.file_selected.connect(_on_export_file_selected)
+
+func _on_export_file_selected(_path):
+	var exportCSV = ExportTable.new()
+	exportCSV.set_file(_path)
+	exportCSV.set_tree_data(root)
+	exportCSV.to_csv()
---
+++ scripts/export_table.gd
@@ -0,0 +1,38 @@
+class_name ExportTable
+
+var file : FileAccess
+var data : TreeItem
+
+func set_file(_path) -> void:
+	self.file = FileAccess.open(_path, FileAccess.WRITE)
+
+func set_tree_data(_root) -> void:
+	self.data = _root
+
+func to_csv():
+	if self.file == null:
+		print("Cannot Open Selected File")
+		return
+	if !self.file.is_open():
+		return
+	var header = ["axis", "starttime", "velocity", "acceleration", "deceleration", "position", "duration"]
+	self.file.store_csv_line(header)
+	var num_points = data.get_child_count()
+	var axes : Dictionary = {"X":2, "Y":3, "Z":4}
+	for i in range(num_points):
+		for axis in axes:
+			var row = PackedStringArray()
+			var pos = data.get_child(i)
+			var vel = data.get_child(i).get_child(0)
+			var acc = data.get_child(i).get_child(1)
+			var dec = data.get_child(i).get_child(2)
+			var dur = data.get_child(i).get_child(3)
+			row.append(axis)
+			row.append(pos.get_text(0))
+			row.append(vel.get_text(axes[axis]))
+			row.append(acc.get_text(axes[axis]))
+			row.append(dec.get_text(axes[axis]))
+			row.append(pos.get_text(axes[axis]))
+			row.append(dur.get_text(axes[axis]))
+			self.file.store_csv_line(row)
+	self.file.close()
